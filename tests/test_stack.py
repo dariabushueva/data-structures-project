5dae96fd3e7c8137249dc0d7200142aad52bc256
dariabushueva <bushueva84@yandex.ru>
2023-05-17 21:51:58 +0300

    added unittests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,28 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack, Node
+
+
+class TestStack(unittest.TestCase):
+
+    def test_node(self):
+        n1 = Node(10, None)
+        n2 = Node(20, n1)
+        self.assertEqual(n1.data, 10)
+        self.assertEqual(n2.data, 20)
+        self.assertEqual(n1.next_node, None)
+
+    def test_stack(self):
+        stack = Stack()
+        self.assertEqual(stack.top, None)
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        self.assertEqual(stack.top.data, 3)
+        self.assertEqual(stack.pop(), 3)
+        self.assertEqual(stack.top.next_node.data, 1)
+        self.assertEqual(stack.pop(), 2)
+        self.assertEqual(stack.top.next_node, None)
+
+
+
+
