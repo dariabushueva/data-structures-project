5ac28846704c6ce02f6b16fa6d84999cf0567324
dariabushueva <bushueva84@yandex.ru>
2023-05-23 21:45:56 +0300

    added __str__ in clsStack, test __str__

--- src/stack.py
+++ src/stack.py
@@ -18,8 +18,18 @@ class Stack:
     def __init__(self):
         """Конструктор класса Stack"""
 
-        # при инициализаци на верху стека пусто
+        # при инициализаци наверху стека пусто
         self.top = None
+
+    def __str__(self):
+        """Возвращает строковое представление стека"""
+
+        current_node = self.top
+        data_list = []
+        while current_node:
+            data_list.append(str(current_node.data))
+            current_node = current_node.next_node
+        return '\n'.join(data_list)
 
     def push(self, data):
         """
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -13,15 +13,20 @@ class TestStack(unittest.TestCase):
 
     def test_stack(self):
         stack = Stack()
+        assert str(stack) == ""
         self.assertEqual(stack.top, None)
         stack.push(1)
         stack.push(2)
         stack.push(3)
+        assert str(stack) == "3\n2\n1"
         self.assertEqual(stack.top.data, 3)
         self.assertEqual(stack.pop(), 3)
         self.assertEqual(stack.top.next_node.data, 1)
         self.assertEqual(stack.pop(), 2)
         self.assertEqual(stack.top.next_node, None)
+
+
+
 
 
 
