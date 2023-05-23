0a293696dcb259ef6976ea1a31b871c85eb5a4d6
dariabushueva <bushueva84@yandex.ru>
2023-05-23 21:48:27 +0300

    added clsNode, clsQueue; added tests

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,13 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        node = Node(data)  # Создаем новый узел
+        if self.head is None:  # Если очередь пуста, новый узел становится головой и хвостом
+            self.head = node
+            self.tail = node
+        else:  # Иначе добавляем новый узел в конец очереди и обновляем хвост
+            self.tail.next_node = node
+            self.tail = node
 
     def dequeue(self):
         """
@@ -31,8 +39,21 @@ class Queue:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.head is None:  # Если очередь пуста, возвращаем None
+            return None
+        head_data = self.head.data  # Запоминаем данные головного элемента
+        self.head = self.head.next_node  # Обновляем голову, переходя к следующему узлу
+        if self.head is None:  # Если после удаления головного элемента очередь оказалась пустой, обновляем хвост
+            self.tail = None
+        return head_data
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+
+        current_node = self.head
+        data_list = []
+        while current_node:
+            data_list.append(str(current_node.data))
+            current_node = current_node.next_node
+        return '\n'.join(data_list)
+
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,33 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Queue, Node
+
+class TestStack(unittest.TestCase):
+
+    def test_node(self):
+        n1 = Node(10, None)
+        n2 = Node(20, n1)
+        self.assertEqual(n1.data, 10)
+        self.assertEqual(n2.data, 20)
+        self.assertEqual(n1.next_node, None)
+
+    def test_queue(self):
+        queue = Queue()
+        self.assertEqual(queue.head, None)
+        self.assertEqual(queue.tail, None)
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        assert str(queue) == "1\n2\n3"
+        self.assertEqual(queue.head.data, 1)
+        self.assertEqual(queue.tail.data, 3)
+        queue.dequeue()
+        self.assertEqual(queue.head.data, 2)
+        self.assertEqual(queue.tail.data, 3)
+        queue.dequeue()
+        self.assertEqual(queue.head.data, 3)
+        self.assertEqual(queue.tail.data, 3)
+        queue.dequeue()
+        self.assertEqual(queue.head, None)
+        self.assertEqual(queue.tail, None)
+        assert str(queue) == ""
